adafb1d5aeab95b4b381bfce6e8376caf6250a9e
Nosima <breno.nosima@gmail.com>
2026-03-20 11:29:41 -0300

    first commit

---
+++ src/main/java/com/bn/categoriacs/CategoriaCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.categoriacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriaCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoriaCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/categoriacs/CategoriaCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.categoriacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriaCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
